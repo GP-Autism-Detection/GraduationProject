6f5985c5fea39b067849ea789abb8319cf02b6e0
EsLam AyMan <eslamayman1214@gmail.com>
2023-02-16 17:59:51 +0200

    Password & email Validation V0.2

--- lib/OTPVerifyToResetPass.dart
+++ lib/OTPVerifyToResetPass.dart
@@ -5,6 +5,7 @@ import 'package:graduation_project_app/ResetPasswordScreen.dart';
 class OTPVerifyToResetPass extends StatelessWidget {
 
   var OtpController = TextEditingController();
+  var FormKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -16,48 +17,60 @@ class OTPVerifyToResetPass extends StatelessWidget {
         padding: const EdgeInsets.all(20.0),
         child: Center(
           child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Image(image: NetworkImage('https://as1.ftcdn.net/v2/jpg/04/42/13/34/1000_F_442133421_UZGoJYPf2cLEPZTBmgeZabNLzgoiAyGg.jpg')),
-                //Text('Login', style: TextStyle(fontSize: 30.0,fontWeight: FontWeight.bold,),),
-                SizedBox(height : 40.0),     // masaf ben login w textbox
-                TextFormField(
-                  controller: OtpController,
-                  decoration: InputDecoration(
-                      labelText: 'Please Enter the Verification Code',    // aw hint text bs bttshal lma tktb
-                      border:OutlineInputBorder(),
-                      prefixIcon: Icon(Icons.verified)                          // icon fl a5er posticon
+            child: Form(
+              key: FormKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image(image: NetworkImage('https://as1.ftcdn.net/v2/jpg/04/42/13/34/1000_F_442133421_UZGoJYPf2cLEPZTBmgeZabNLzgoiAyGg.jpg')),
+                  //Text('Login', style: TextStyle(fontSize: 30.0,fontWeight: FontWeight.bold,),),
+                  SizedBox(height : 40.0),     // masaf ben login w textbox
+                  TextFormField(
+                    controller: OtpController,
+                    decoration: InputDecoration(
+                        labelText: 'Please Enter the Verification Code',    // aw hint text bs bttshal lma tktb
+                        border:OutlineInputBorder(),
+                        prefixIcon: Icon(Icons.verified)                          // icon fl a5er posticon
+                    ),
+                    keyboardType: TextInputType.number,
+                    onFieldSubmitted: (String value){            // aw onchanged be return ay change
+                      print(value);
+                    },
+                    validator: (value){
+                      if(value!.isEmpty){
+                        return 'Verification Code is Required !';
+                      }
+                      return null;
+                    },
                   ),
-                  keyboardType: TextInputType.number,
-                  onFieldSubmitted: (String value){            // aw onchanged be return ay change
-                    print(value);
-                  },
-                ),
-                // space fe el col --> height
+                  // space fe el col --> height
 
-                SizedBox( height: 15,),
-                Container(
-                  width: double.infinity,
-                  color: Colors.blue,
-                  child: MaterialButton(onPressed: (){
-                    print(OtpController.text);
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => ResetPasswordScreen()),
-                    );
+                  SizedBox( height: 15,),
+                  Container(
+                    width: double.infinity,
+                    color: Colors.blue,
+                    child: MaterialButton(onPressed: (){
+                      if(FormKey.currentState!.validate() ) {
+                        print(OtpController.text);
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => ResetPasswordScreen()),
+                        );
 
-                  },
-                    child: Text(
-                      'Verify',           // mafe4 width so wrap to container
-                      style: TextStyle(
-                        color: Colors.white,
+                      }
+
+                    },
+                      child: Text(
+                        'Verify',           // mafe4 width so wrap to container
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
                       ),
                     ),
-                  ),
-                ),                 // onPressed ---> annonumse func --> (){}
+                  ),                 // onPressed ---> annonumse func --> (){}
 
-              ],
+                ],
+              ),
             ),
           ),
         ),
