554451375c964bef1e04c7a84e8c0bdc828771fc
AhmedAbdElhamed909 <65867568+AhmedAbdElhamed909@users.noreply.github.com>
2023-02-18 11:21:59 +0200

    Add files via upload

---
+++ lib/TestScreen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class TestScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(
+          Icons.menu,
+        ),
+        title: Text(
+          'Autism Detection',
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children:[
+            Image(
+              image: NetworkImage('https://domf5oio6qrcr.cloudfront.net/medialibrary/13292/700fc35b-3c70-4762-b24a-c9d895eb9863.jpg'),
+              height: 200.0,
+              width: 200.0,
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Container(
+              width: double.infinity,
+              color: Colors.indigoAccent,
+              child: MaterialButton(onPressed: ()
+              {
+                print('Result');
+              },
+                child: Text(
+                  'Show result',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
